c3f934742e8b2607767d8e10e42c255902d80af8
Gustav Lund <Tugsav@gmail.com>
2015-04-23 16:48:48 +0200

    Data extraction v. 2
    Category download now returns a dict of categories with lists of pages
    (or subcats) as items
    The page downloader is updated to take such a dict as input along with a
    list of downloaded pages to avoid double downloads. Support functions
    have been added for this, and the script has an example of how to use
    this to download all the pages over several sessions

--- DataExtractor.py
+++ DataExtractor.py
@@ -32,12 +32,16 @@ debugar = []
 def query(request):
     request['action'] = 'query'
     request['format'] = 'json'
+    request['cmlimit'] = 'max'
     lastContinue = {'continue': ''}
     while True:
         req = request.copy()
         req.update(lastContinue)
         result = requests.get('http://starwars.wikia.com/api.php',params=req).json()
         debugar.append(result)
+        if 'query' in result:        
+            if 'categorymembers' in result['query']:
+                print len(result['query']['categorymembers'])
         if 'query' in result: yield result['query']
         if 'continue' not in result:
             if 'query-continue' not in result:            
@@ -70,34 +74,29 @@ def flatten(result):
 
 
 ###Recursive function for category and subcategory download
-def downloadWikiCategory(name):
+def downloadWikiCategory(name,oldresults):
     content = []
     for result in query({'list':'categorymembers','cmtitle':name}):
         content.append(result)
         
     flattened = flatten(content)
     
-    result = dict()
-    #subcats = []
-    #doombox = []
+    result = oldresults
+    result[name]=flattened
     for stuff in flattened:        
         if 'Category:' in stuff['title']:
-            result[stuff['title']] = downloadWikiCategory(stuff['title'])
-            ##doombox.append(stuff)
-        else:
-            result[stuff['title']] = stuff['pageid']
-    #print "Subcategories in "+name+": "+str(len(subcats))
-    #for result in subcats:
-    #    res = res + result
-
-    #for destroyer in doombox:
-    #    res.remove(destroyer)
+            if not (stuff['title'] in result.keys()):           
+                print stuff['title']
+                subres = downloadWikiCategory(stuff['title'],result)
+                result = dict(result,**subres)
+#        else:
+#            result[stuff['title']] = stuff['pageid']
     
     return result         
 
-print 'Downloading category'        
-test = downloadWikiCategory('Category:Clones')
-print 'Finished Downloading category' 
+#print 'Downloading category'        
+#test = downloadWikiCategory('Category:Star_Wars',dict())
+#print 'Finished Downloading category' 
 
 def getPage(name):
     response = urllib2.urlopen('http://starwars.wikia.com/api.php?format=json&action=query&titles='+name+'&prop=revisions&rvprop=content')
@@ -125,43 +124,110 @@ def getURL(title):
             result=result+char
     return result
 
-def getCategoryPages(category,downloadedkeys=[],depth=0):   
-    print 'at depth: '+str(depth)    
+def getCategoryPages(category_dict,downloadedkeys=[],depth=0):     
     canonPages = dict()
     nonCanonPages = dict()
     hasNoCanon = dict()
-    for page in category:
-        if not page in downloadedkeys:        
-            if 'Category:' in page:
-                print 'Now downloading '+page             
-                subResult = getCategoryPages(category[page],downloadedkeys,depth+1)
-                canonPages = dict(canonPages,**subResult['Canon'])
-                nonCanonPages = dict(nonCanonPages,**subResult['nonCanon'])
-                hasNoCanon = dict(hasNoCanon,**subResult['hasNoCanon'])
-            else:
-                downloadedkeys.append(page)
-                canonPage = getPage(getURL(page+'/Canon'))
-                if not isMissing(canonPage):
-                    canonPages[page]=canonPage
-                    nonCanonPages[page]=getPage(getURL(page))
+    print len(downloadedkeys)
+    for category in category_dict:
+        print category + ' of size ' + str(len(category_dict[category]))
+        Ghostlings = 0        
+        for page_dict in category_dict[category]:
+            page = page_dict['title']
+            if not page in downloadedkeys:        
+                if 'Category:' in page:
+                    Ghostlings = Ghostlings + 1
+    #                print 'Now downloading '+page             
+    #                subResult = getCategoryPages(category[page],downloadedkeys,depth+1)
+    #                canonPages = dict(canonPages,**subResult['Canon'])
+    #                nonCanonPages = dict(nonCanonPages,**subResult['nonCanon'])
+    #                hasNoCanon = dict(hasNoCanon,**subResult['hasNoCanon'])
                 else:
-                    hasNoCanon[page]=getPage(getURL(page))
+                    downloadedkeys.append(page)
+                    canonPage = getPage(getURL(page+'/Canon'))
+                    if not isMissing(canonPage):
+                        canonPages[page]=canonPage
+                        nonCanonPages[page]=getPage(getURL(page))
+                    else:
+                        hasNoCanon[page]=getPage(getURL(page))
+            else:
+                Ghostlings = Ghostlings + 1
+        print 'Amount of Ghosting pages: '+str(Ghostlings)
     result = dict()
     result['Canon']=canonPages
     result['nonCanon']=nonCanonPages
     result['hasNoCanon']=hasNoCanon
+    print len(downloadedkeys)
     return result
 
 #test2 = getPagesWithCanon(test[1300:])
-print 'Downloading pages'
-test2 = getCategoryPages(test)
+#print 'Downloading pages'
+#test2 = getCategoryPages(test)
 
 ##Save dat shite
 import unicodedata
 import codecs
-fileObject = codecs.open('CategoryMapData','w','utf-8-sig')
-json.dump(test,fileObject)
+#fileObject = codecs.open('CategoryMapDataFull','w','utf-8-sig')
+#json.dump(test,fileObject)
+#fileObject.close()
+#fileObject = codecs.open('PageDataFull','w','utf-8-sig')
+#json.dump(test2,fileObject)
+#fileObject.close()
+
+##Load it again
+fileObject = codecs.open('CategoryMapDataFull','r','utf-8-sig')
+category_data = json.load(fileObject)
 fileObject.close()
-fileObject = codecs.open('PageData','w','utf-8-sig')
-json.dump(test2,fileObject)
+
+##Splitter function to get subset of the dict
+def subdict(data,index_start,index_end):
+    result = dict()
+    maxcount = 0    
+    for i in range(index_start,index_end+1):
+        key = data.keys()[i]
+        value = data[key]
+        result[key]=value
+        maxcount = maxcount + len(value)
+    print maxcount
+    return result
+    
+
+##Download pages
+##Root category
+#category_data['Category:Star_Wars']
+
+subdata = subdict(category_data,0,22)
+##Category 23 "Redirects from alternate spelling" includes SERIOUSLY weird chars
+##and are useless pages anyway. Leave it aloooone
+page_data = getCategoryPages(subdata)
+
+def extractKnownKeys(data_dict):
+    result = []
+    for key in data_dict['nonCanon']:
+        result.append(key)
+    for key in data_dict['hasNoCanon']:
+        result.append(key)
+    return result
+
+fileObject = codecs.open('PageDataFull','w','utf-8-sig')
+json.dump(page_data,fileObject)
 fileObject.close()
+
+##Load it again
+fileObject = codecs.open('PageDataFull','r','utf-8-sig')
+page_data = json.load(fileObject)
+fileObject.close()
+
+##Result merging function
+def mergeResults(result1,result2):
+    result = dict()    
+    result['Canon'] = dict(result1['Canon'],**result2['Canon'])
+    result['nonCanon'] = dict(result1['nonCanon'],**result2['nonCanon'])
+    result['hasNoCanon'] = dict(result1['hasNoCanon'],**result2['hasNoCanon'])
+    return result
+
+##Download more pages
+knownKeys=extractKnownKeys(page_data)
+page_data2 = getCategoryPages(subdata,knownKeys)
+page_data = mergeResults(page_data,page_data2)
+#####Now rinse and repeat untill all pages have been downloaded (or attempt downloading all at once)

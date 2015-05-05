deb470da6daf2b1c77212c3a122dd85436d9a905
Gustav Lund <Tugsav@gmail.com>
2015-05-05 12:10:14 +0200

    Data filtering update v2
    EVEN more filtering cases. Also included is a cleaning function that
    horribly cuts the pages down to almost nothing

--- DataFiltering.py
+++ DataFiltering.py
@@ -10,6 +10,7 @@ import json
 import requests
 import unicodedata
 import codecs
+import re
 
 #Load category data
 fileObject = codecs.open('CategoryMapDataFull','r','utf-8-sig')
@@ -20,6 +21,50 @@ fileObject.close()
 fileObject = codecs.open('PageDataFull','r','utf-8-sig')
 page_data = json.load(fileObject)
 fileObject.close()
+
+#Getting content of page
+def getContent(page):
+    p=page['query']['pages'].values()[0]
+    return p['revisions'][0]['*']
+   
+#Getting a dict of the character box
+def getCharBox(content):
+    mess = re.findall(r'\{\{Character\n[^\}\}]*\}\}',content)[0]
+    linemess = re.findall(r'\n\|[^\n]*',mess)  
+    linemess[-1] = re.sub(r'([^\}\}])\}\}*','\g<1>',linemess[-1])
+    lines = dict()
+    for line in linemess:
+        val = line.split('=')[1]        
+        if(val!=''):
+            key = re.findall('\n\|([^=]*)=',line)[0]
+            lines[key]=val
+    return lines
+   
+#Cleaning the content of unwanted symbols and syntaxing
+def cleanContent(content):
+    tempdict=getCharBox(content)    
+    tempcontent = content    
+    for key in tempdict:
+        tempcontent += ' '+tempdict[key]
+    #Removing tags
+    iteration1 = re.sub(r'\{\{[^}]*\}\}','',tempcontent)
+    #Removing Boldface syntax    
+    iteration2 = re.sub(r'\'\'\'([^\'\'\']*)\'\'\'','\g<1>',iteration1) 
+    #Removing Italic syntax
+    iteration3 = re.sub(r'\'\'([^\'\']*)\'\'','\g<1>',iteration2)
+    #Removing Category tags
+    iteration4 = re.sub(r'\[\[Category:[^\]\]]*\]\]','',iteration3)
+    #Remove subsection syntax
+    iteration5 = re.sub(r'==([^==]*)==','\g<1>',iteration4)
+    #Remove "Safe" symbols
+    iteration6 = re.sub(r'\*|"','',iteration5)
+    #Unpack internal links
+    iteration7 = re.sub(r'\[\[([^\]\]]*)\]\]','\g<1>',iteration6)
+    #remove linespaces
+    iteration8 = re.sub(r'\n',' ',iteration7)
+    #remove multiple spaces
+    iteration9 = re.sub(r' +',' ',iteration8)
+    return iteration9
 
 #Filtering files out
 def isFilePage(page):
@@ -32,41 +77,46 @@ print isFilePage(page_data['hasNoCanon'].values()[6])
 
 #Filtering redirects out
 def isRedirectPage(page):
-    p=page['query']['pages'].values()[0]
-    content = p['revisions'][0]['*']
+    content = getContent(page)
     way1 = ('#REDIRECT' in content)
     way2 = ('#redirect' in content)
     return way1 or way2
 
 #Disambiguation page filering
 def isDisambiguationPage(page):
-    p=page['query']['pages'].values()[0]
-    content = p['revisions'][0]['*']
+    content = getContent(page)
     way1 = ('{{Disambig}}' in content)
     way2 = ('{{disambig}}' in content)
     way3 = ('[[Category:Disambiguation pages]]' in content)
     return way1 or way2 or way3
 
 def isStubPage(page):
-    p=page['query']['pages'].values()[0]
-    content = p['revisions'][0]['*']
+    content = getContent(page)
     way1 = '{{stub}}' in content
-    return way1
+    way2 = '{{Stub}}' in content
+    way3 = '{{Ship-stub}}' in content
+    return way1 or way2 or way3
 
 #Searching for other stub page stuff
-for key in page_data['hasNoCanon']:
-    p=page_data['hasNoCanon'][key]['query']['pages'].values()[0]
+checkdict = page_data['hasNoCanon']
+count = 0
+for key in checkdict:
+    p=checkdict[key]['query']['pages'].values()[0]
     title = p['title']
-    if isFilePage(page_data['hasNoCanon'][key]):
+    if isFilePage(checkdict[key]):
         continue
     if not 'revisions' in p.keys():
         print 'failing:    '+title
         print p
         continue
-    if isRedirectPage(page_data['hasNoCanon'][key]) or isDisambiguationPage(page_data['hasNoCanon'][key]):     
+    if isRedirectPage(checkdict[key]):
+        continue
+    if isDisambiguationPage(checkdict[key]):     
+        continue
+    if isStubPage(checkdict[key]):
         continue
     content = p['revisions'][0]['*']
-    if len(content) < 150:
+    if len(cleanContent(content)) < 100:
         print title
-
-page_data['hasNoCanon']['File:Heroes']
+    count+=1
+print "Pages after filtering "+str(count)

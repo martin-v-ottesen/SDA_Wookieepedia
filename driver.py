80c339ccbadb4c69e928cc66bf619991e45ddb03
s103451 <s103451@student.dtu.dk>
2015-05-10 17:22:47 +0200

    Improvements to driver
    Hist in driver

--- driver.py
+++ driver.py
@@ -7,44 +7,29 @@ import json
 import codecs
 import DataFiltering
 import matplotlib.pyplot as plt
+import math
+import matplotlib.patches as mpatches
 
-dataFile = codecs.open('PageDataMerge(0-2500)', 'r', 'utf-8-sig')
-
-data = json.load(dataFile)
-
-dataFile.close()
-
-print len(data.keys())
-print data.keys()
-
-print "Canon: " + str(len(data['Canon'].keys()))
-print "nonCanon: " + str(len(data['nonCanon'].keys()))
-print "hasNoCanon: " + str(len(data['hasNoCanon'].keys()))
-
-clean_canon = DataFiltering.filterdata(data['Canon'])
-# clean_non_canon = DataFiltering.filterdata(data['nonCanon'])
-# clean_hasNo_canon = DataFiltering.filterdata(data['hasNoCanon'])
-
-# print clean_canon.keys()
-
-
-
-print clean_canon['Gungan/Canon']
-exit()
+# --- Load data ---
+with codecs.open('CleanPageDataTest3(0-4371)', 'r', 'utf-8-sig') as dataFile:
+    data = json.load(dataFile)
 
 
 def wordCount(txt):
     return len(txt.split())
 
-word_count = []
+# --- Histogram ---
+counter = {'Canon':[], 'nonCanon':[], 'hasNoCanon':[]}
 
-for page in clean_canon:
-    word_count.append(wordCount(page))
+for cat in data.keys():
+    for page in data[cat].keys():
+        counter[cat].append(math.log(wordCount(data[cat][page])))
 
-
-print len(word_count)
-print word_count
-
-plt.hist(word_count, bins=50)
+plt.hist(counter['Canon'], bins=20)
+canon_patch = mpatches.Patch(color='blue', label='Canon')
+plt.hist(counter['nonCanon'], bins=20)
+legend_patch = mpatches.Patch(color='green', label='Legend')
+plt.legend(handles=[canon_patch, legend_patch])
+plt.ylabel("# of pages")
+plt.xlabel("log(word count)")
 plt.show()
-
